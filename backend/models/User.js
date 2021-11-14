8f3d9282861152ddc1dfb12f030f7ff3e8b9ec1a
sumitra1 <sumitrakumarishaw1@gmail.com>
2021-11-14 17:32:53 +0530

    added data validation

--- backend/models/User.js
+++ backend/models/User.js
@@ -20,5 +20,7 @@ const UserSchema = new Schema({
         default: Date.now
     },
   });
+const User=mongoose.model('user',UserSchema);
+User.createIndexes();
+module.exports=User;
 
-  module.exports = mongoose.model('user', UserSchema);
