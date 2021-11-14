88adf73d99728aa5ceecd5985ac96a2a28ddaec8
sumitra1 <sumitrakumarishaw1@gmail.com>
2021-11-14 16:25:39 +0530

    storing data into database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,14 @@
 const express=require('express');
+const User = require('../models/User');
 
 const router=express.Router();
 
-router.get('/',(req,res)=>{
-    obj={
-        a:'thios',
-        number:34
-    }
-    res.json(obj)
+
+//create a user using :POST "/api/auth/" .does not require auth
+router.post('/',(req,res)=>{
+   console.log(req.body);
+   const user=User(req.body);
+   user.save();
+   res.send(req.body);
 })
 module.exports=router
