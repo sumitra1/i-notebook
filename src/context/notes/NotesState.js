7596d818cb9d457cd577e4d87d23d78062b298f5
sumitra1 <sumitrakumarishaw1@gmail.com>
2021-11-17 21:00:30 +0530

    fetching notes

--- src/context/notes/NotesState.js
+++ src/context/notes/NotesState.js
@@ -1,22 +1,60 @@
 
 import NoteContext from "./noteContext";
-import {useState} from 'react'
+import { useState } from "react";
+
 const NoteState=(props)=>{
-    const s1={
-        "name":"Sumitra",
-        "class":"8"
-    }
-    const [state, setstate] = useState(s1);
-    const update=()=>{
-        setTimeout(()=>{
-            setstate({
-                "name":"Kavita",
-                "class":"10"
-            })
-        },1000);
-    }
+    
+    
+        const notesInitial=[
+        {
+          "_id": "619256b5ab36cf4701144309",
+          "user": "6191ddcdc4c99e6b21afcc67",
+          "title": "my first note",
+          "description": "please write something.",
+          "tag": "personal",
+          "date": "2021-11-15T12:46:45.521Z",
+          "__v": 0
+        },
+        {
+          "_id": "61925766ab36cf470114430b",
+          "user": "6191ddcdc4c99e6b21afcc67",
+          "title": "my first note",
+          "description": "please write something.",
+          "tag": "personal",
+          "date": "2021-11-15T12:49:42.995Z",
+          "__v": 0
+        },
+        {
+          "_id": "6192577fab36cf470114430d",
+          "user": "6191ddcdc4c99e6b21afcc67",
+          "title": "my first note",
+          "description": "please write something.",
+          "tag": "personal",
+          "date": "2021-11-15T12:50:07.240Z",
+          "__v": 0
+        },
+        {
+          "_id": "61925780ab36cf470114430f",
+          "user": "6191ddcdc4c99e6b21afcc67",
+          "title": "my first note",
+          "description": "please write something.",
+          "tag": "personal",
+          "date": "2021-11-15T12:50:08.663Z",
+          "__v": 0
+        },
+        {
+          "_id": "61925b687a6f7dc237bd4a53",
+          "user": "6191ddcdc4c99e6b21afcc67",
+          "title": "my first note",
+          "description": "please write something.",
+          "tag": "personal",
+          "date": "2021-11-15T13:06:48.203Z",
+          "__v": 0
+        }
+      ]
+      const [notes, setNotes] = useState(notesInitial)
     return(
-        <NoteContext.Provider value={{state,update}}>
+        <NoteContext.Provider value={{notes,setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
